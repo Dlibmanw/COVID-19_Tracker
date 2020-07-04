904412c26056fde6d08dfa8720e5d71b459a81b6
Dafna Libman <dafnalibman@gmail.com>
2020-07-04 17:47:53 +0100

    Add countup feature

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import styles from './Cards.module.css';
- 
+import CountUp from 'react-countup' 
+
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdated}}) => {
   console.log(confirmed)
   if(!confirmed){
@@ -15,7 +16,9 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdated}}) => {
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Infected</Typography>
-            <Typography variant="h5">{ confirmed.value }</Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={confirmed.value} duration={2.5} separator=","/>
+              </Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
           </CardContent>
