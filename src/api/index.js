3e695902495168159ba7a1d245c5f2fd43b03883
Dafna Libman <dafnalibman@gmail.com>
2020-07-04 13:50:09 +0100

    Update fetchData, get only the required data

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,17 @@ const url = 'https://covid19.mathdro.id/api'
 
 export const fetchData = async () => {
   // here, we are going to have an async function. We are going to deal with it using async. 
-  // async deals with promises the same way .then and .catch does, but it's easier to read and write.
+  // async deals with promises the same way '.then' and '.catch' does, but it's easier to read and write.
   // try is going to be executed if the fetch is successful, otherwise we're going to get to the catch block.
   try {
-    const response = await axios.get(url)
-    return response;
+    const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url)
+    const modifiedData = {
+      confirmed: confirmed,
+      recovered: recovered,
+      deaths: deaths,
+      lastUpdate: lastUpdate
+    }
+    return modifiedData ;
   } catch (error) {
 
   }
