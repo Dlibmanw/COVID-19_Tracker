4eb7699339c82d9b4f46bdd26c810fe58120d597
Dafna Libman <dafnalibman@gmail.com>
2020-07-04 17:20:24 +0100

    Update cards. Creating structure for the first card

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import { fetchData } from './api'
 
 class App extends React.Component {
   // the best place to fetch the data inside the class based component is inside: componentDidMount
+  state = {
+    data: {}
+  }
   async componentDidMount() {
-    const data = await fetchData();
-
-    console.log(data);
+    const fetchedData = await fetchData();
+    // populating data with data:
+    this.setState({ data: fetchedData })
   }
 
   render()  {
+    const { data } = this.state;
     return (
       <div className={ styles.container }>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
